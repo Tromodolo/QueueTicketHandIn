26bdce0b7ddb44e06524e104d7219b530330c452
Tromodolo <Cazionchallosette@gmail.com>
2017-10-17 13:52:16 +0200

    Initial add of files

---
+++ QueueTicketDispenser.java
@@ -0,0 +1,15 @@
+package Kölappsutdelare1;
+
+public class QueueTicketDispenser {
+    private int nr;
+    
+    public QueueTicketDispenser(){
+        nr = 1;
+    }
+    
+    public int getNextNumber(){
+        return nr++;
+    }
+    
+    
+}
