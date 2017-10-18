e8a2ae50dc9784e3405ba2f84fc5e47e6c9c39b4
Tromodolo <Cazionchallosette@gmail.com>
2017-10-18 14:02:27 +0200

    Edit queues to have two queues have the same number

--- Utdelare_av_kölappar.java
+++ Utdelare_av_kölappar.java
@@ -3,18 +3,17 @@ package Kölappsutdelare1;
 public class Utdelare_av_kölappar {
 
     public static void main(String[] args) {
-        QueueTicketDispenser q = new QueueTicketDispenser();
-        int myNumber = q.getNextNumber();
-        System.out.println("Könummer: " + myNumber);
-        
-        
-        //Vad händer om vi skapar ett objekt till?
-        //QueueTicketDispenser q2 = new QueueTicketDispenser();
-        myNumber = q.getNextNumber();
-        System.out.println("Könummer: " + myNumber);
-        //Det andra objektet levererar samma serie nummer som det första,
-        //lösning: skapa bara ett objekt av klassen
-        
+        //First queue
+        QueueTicketDispenser q1 = new QueueTicketDispenser();
+
+        int myNumber = q1.getNextNumber();
+        System.out.println("Könummer a: " + myNumber);
+
+        //Second Queue
+        QueueTicketDispenser q2 = new QueueTicketDispenser();
+
+        myNumber = q2.getNextNumber();
+        System.out.println("Könummer b: " + myNumber);
       
     }
 }
